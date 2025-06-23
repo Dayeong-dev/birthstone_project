9005e52487efa415e5dd370d50cc50385410bf99
Dayeong-dev <ydiosa98@gmail.com>
2025-06-23 17:00:18 +0900

    feat: Birthstone 등록/수정 메서드 구현 및 테스트 추가

--- src/main/java/com/example/birthstone/dao/IBirthstoneDao.java
+++ src/main/java/com/example/birthstone/dao/IBirthstoneDao.java
@@ -2,10 +2,12 @@ package com.example.birthstone.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.birthstone.dto.BirthstoneDTO;
 
@@ -20,4 +22,10 @@ public interface IBirthstoneDao {
 	
 	@Insert("INSERT INTO tbl_birthstone(MONTH, NAME, ENGNAME, DESCRIPTION, Image_url) VALUES (#{bs.month}, #{bs.name}, #{bs.engName}, #{bs.description}, #{bs.imageURL})")
 	public void setBirthstone(@Param("bs") BirthstoneDTO bs);
+	
+	@Update("UPDATE tbl_birthstone SET NAME = #{name} WHERE MONTH = #{month}")
+	public void updateBirthstoneName(@Param("name") String name, @Param("month") Integer month);
+	
+//	@Delete("DELETE FROM tbl_birthstone WHERE MONTH = #{month}")
+//	public void deleteBirthstone(@Param("month") Integer month);
 }
