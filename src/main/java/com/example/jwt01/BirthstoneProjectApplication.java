8ccbf8be4a558b517a42de37d629603e5b7184ec
Dayeong-dev <ydiosa98@gmail.com>
2025-06-23 12:35:22 +0900

    first commit

---
+++ src/main/java/com/example/jwt01/BirthstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthstoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthstoneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt01/BirthstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
